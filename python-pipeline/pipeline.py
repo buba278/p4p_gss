b9cfcd92fb71ff0cbf846b78fe13a26ed352e782
Christian Motta <chrismotta2005@gmail.com>
2026-05-14 11:50:07 +1200

    Conversion from velocity to doppler peak freq, signal generated

--- python-pipeline/pipeline.py
+++ python-pipeline/pipeline.py
@@ -9,20 +9,26 @@ SAMPLE_RATE = 50000 # ADC rate - nyquist 25kHz
 # velocity resolution = 48.8 * C / (2 * CARRIER * cos(45)) = ~0.43 m/s per bin
 FFT_SIZE = 1024 # (accuracy vs latency)
 
-CARRIER = 24e9 # 24GHz
-MOUNTING_ANGLE = 45 # degrees
+CARRIER_FREQ = 24e9
+MOUNT_ANGLE_DEG = 45
+MOUNT_ANGLE_RAD = np.radians(MOUNT_ANGLE_DEG)
 C = 3e8 # speed of light
+
+def velocity_to_doppler(velocity_ms):
+    # find what peak freq to expect in spectrum
+    # fd = 2v * f0 * cos(theta) / c
+    return (2 * velocity_ms * CARRIER_FREQ * np.cos(MOUNT_ANGLE_RAD)) / C
 
 def make_test_signal(velocity_ms, noise_amplitude=0.0, n_samples=FFT_SIZE):
     # seconds axis
     t = np.arrange(n_samples) / SAMPLE_RATE
     
     # IF signal is sine wave at CARRIER freq
-    # TODO: get freq shift for a given velocity
-    # TODO: create sine wave
+    target_freq = velocity_to_doppler(velocity_ms)
+    clean_signal = np.sin(2 * np.pi * target_freq * t)
 
     # fake gaussian noise
     noise = np.random.normal(0, noise_amplitude, n_samples)
 
-    return noise
+    return clean_signal + noise, target_freq
 
