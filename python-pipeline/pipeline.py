8991c844ea9db515bfb68a3d306d104ee18cae92
Christian Motta <chrismotta2005@gmail.com>
2026-05-17 12:17:08 +1200

    Pipeline implementation with XCA and CFAR

--- python-pipeline/pipeline.py
+++ python-pipeline/pipeline.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy as sp
 
 # Sampling at ~108kHz
 # f = 2 * 24e9 * 30 * cos(45deg) / 3e8 = ~3394 Hz
@@ -36,6 +37,52 @@ def make_test_signal(velocity_ms, noise_amplitude=0.0, n_samples=FFT_SIZE):
 
     return clean_signal + noise, target_freq
 
-print(f"5 m/s -> {velocity_to_doppler(5):.1f} Hz")
-print(f"10 m/s -> {velocity_to_doppler(10):.1f} Hz")
-print(f"30 m/s -> {velocity_to_doppler(30):.1f} Hz")
+def process_frame(samples):
+    n = len(samples)
+
+    # -- hamming window --
+    # tapering coefficients (will have same coefficients in ROM)
+    window = np.hamming(n)
+    windowed_samples = samples * window
+
+    # -- fft --
+    # rfft folds results from n to n/2 + 1
+    # get freq magnitude bins - bins from 0 -> SAMPLE_RATE/2 (nyquist)
+    freq_spectrum = np.abs(np.fft.rfft(windowed_samples))
+    # map each bin index to a frequency (Hz)
+    freq_axis = np.fft.rfftfreq(n, d=1.0/SAMPLE_RATE)
+
+    # -- CFAR (adaptive noise floor) --
+    bin_count = len(freq_spectrum)
+    # decide threshold based off of estimated surrounding noise floor, per each bin
+    guard_bins = 4 # num bins adjacent to candidate - to be ignored (could have signal energy)
+    ref_bins = 16  # num bins around guard bins used to estimate noise floor
+    cfar_threshold = np.zeros_like(freq_spectrum)
+
+    # calculate threshold for each bin i
+    for i in range(bin_count):
+        # slice ref bins width - excl guard bins
+        left_refs = freq_spectrum[max(0, i-guard_bins-ref_bins):max(0, i-guard_bins)]
+        right_refs = freq_spectrum[min(bin_count, i+guard_bins+1):min(bin_count, i+guard_bins+ref_bins+1)]
+        all_refs = np.concatenate([left_refs, right_refs])
+
+        if (len(all_refs > 0)):
+            # threshold = arbitrarily 2x local average
+            cfar_threshold[i] = 2 * np.mean(all_refs)
+
+    # clear all bins that dont meet local threshold
+    freq_spectrum = np.where(freq_spectrum > cfar_threshold, freq_spectrum, 0)
+
+    # -- XCA peak detection --
+    gaussian_width = 10 # tune based off antenna width
+    gaussian_x = np.arrange(-3*gaussian_width, 3*gaussian_width + 1)
+    reference_gaussian = np.exp(-0.5 * (gaussian_x / gaussian_width)**2)
+
+    # cross correlation - slide curve across spectrum
+    # output peaks where shapes align the best
+    correlation = sp.correlate(freq_spectrum, reference_gaussian, mode='same')
+    peak_bin = np.argmax(correlation)
+    peak_freq = freq_axis[peak_bin]
+    velocity = doppler_to_velocity(peak_freq)
+    
+    return velocity, freq_axis, freq_spectrum, correlation, peak_freq
